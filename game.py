a202f70223af31fcea336ef39fba4cdbddee0f90
mitch@skymachine.com <mitch@skymachine.com>
2023-05-07 05:47:29 -0700

    Update game.py

--- game.py
+++ game.py
@@ -18,40 +18,62 @@
 #need to add a way to get a list of spells.
 
 
+
+#character creation
+
 username = input("Enter username:")
-
 character = []
-
 print("Username is: " + username)
-
 character.append(username)
-
 job =input("Enter job:")
-
 character.append(job)
-
 print("Job is: " + job)
-
 print(character)
 
-command = input("Enter command:")
+#location creation
+room = "first room"
 
-if command == "look room":
-    print("You are in a garden village, the air is fresh and the sun is shining.")
-    print("There is a well in the middle of the village. There is a path to the north.")
-else:
-    print("I don't understand that.")
+#game loop
 
-command = input("Enter command:")
+#functions
 
-if command == "look well":
-    print("You look down the well and see a lost cat.")
-else:
-    print("I don't understand that.")   
-   
-command = input("What do you do:")
+def first_level():
+    global room
+    room = "first room"
     
-if command == "get cat":
-    print("You got the cat! " + username + " wins!")
-else:
-    print("I don't understand that.")
+    command = input("Enter command:")
+    if room == "first room" and command == "look room":
+        print("You are in a garden village, the air is fresh and the sun is shining.")
+        print("There is a well in the middle of the village. There is a path to the north.")
+        first_level()
+    if room == "first room" and command == "go north":
+        print("You go north to the Cathedral.")
+        room = "second room"
+        second_level()
+    if room == "first room" and command == "look well":
+        print("You look down the well and see a lost cat.")
+        first_level()
+    if command == "get cat":
+        print("You got the cat! " + username + " adds cat to inventory.")
+        first_level()
+    else:
+        print("I don't understand that.")
+        first_level()
+
+def second_level():
+    global room
+    command = input("Enter command:")
+    if room == "second room" and command == "look room":
+        print("You are in a cathedral. There is a path to the south.")
+        second_level()
+    if room == "second room" and command == "go south":
+        print("You go south to the Village Center.")
+        first_level()
+    else:
+        print("I don't understand that.")
+        second_level()
+
+    
+
+#start game
+first_level()
