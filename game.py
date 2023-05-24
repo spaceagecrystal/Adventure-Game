d121454af54066e7915ba450bd3f66bf33fdbda3
Mitch Greer <mitch@skymachine.com>
2023-05-24 07:19:13 -0700

    edit todo

--- game.py
+++ game.py
@@ -15,7 +15,7 @@
 #need to add a way to get a list of armor.
 #need to add a way to get a list of spells.
 #need to add a a way to randomly generate sa monster.
-#work on database conncection. By end of weekend
+#work on database conncection. 
 
 #Dice rolling system
 
